68f3f76131b11c102e50b13fb3d25eda90aa0fb9
huanggang19980622 <471958975@qq.com>
2019-08-12 20:26:23 +0800

    删除了

--- shoedog-Django-website-master/activities/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Activity
-# Register your models here.
-@admin.register(Activity)
-class ActivityAdmin(admin.ModelAdmin):
-    list_display = ('id','name','palace','start_time','end_time','detail')
--- shoedog-Django-website-master/comment/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Commment
-# Register your models here.
-@admin.register(Commment)
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ('id','content_object','text','comment_time','user')
